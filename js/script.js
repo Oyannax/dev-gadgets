38c6ce3d8299905f0d4fec4c870bc974c4566906
Oyanna Lopez <oyannalopez14@gmail.com>
2023-09-19 11:36:27 +0200

    display img when mouse over + improv

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 const picHandlerBtns = document.querySelectorAll(".pictures-handler");
+const imgList = document.querySelector(".thumbs");
 const displayedPic = document.querySelector(".pictures-img");
-let counter = 1;
+let imgData = 1;
 
 picHandlerBtns.forEach(btn => {
     btn.addEventListener('click', function(event) {
         if (event.target.classList.contains("pictures-prev")) {
-            counter--;
-            if (counter < 1) counter = 5;
-            displayedPic.src = `img/canard-jaune-${counter}-l.png`;
+            imgData--;
+            if (imgData < 1) imgData = 5;
+            displayedPic.src = `img/canard-jaune-${imgData}-l.png`;
         } else if (event.target.classList.contains("pictures-next")) {
-            counter++;
-            if (counter > 5) counter = 1;
-            displayedPic.src = `img/canard-jaune-${counter}-l.png`;
+            imgData++;
+            if (imgData > 5) imgData = 1;
+            displayedPic.src = `img/canard-jaune-${imgData}-l.png`;
         }
     })
+})
+
+imgList.addEventListener('mouseover', function(event) {
+    if (event.target.classList.contains("thumbs-img")) {
+        imgData = event.target.dataset.img;
+        displayedPic.src = `img/canard-jaune-${imgData}-l.png`;
+    }
 })
